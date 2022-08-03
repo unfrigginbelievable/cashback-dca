7f82fd2efd10abe85814ff8a556120f4760d043b
unfrigginbelievable <unfrigginbelievable117@gmail.com>
2022-08-02 21:18:57 -0400

    Got usdc borrow working

--- src/AaveBot.sol
+++ src/AaveBot.sol
@@ -9,16 +9,24 @@ import "openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "prb-math/contracts/PRBMathUD60x18.sol";
 
 contract AaveBot {
+    uint256 public constant LTV_BIT_MASK = 65535;
+    uint256 public constant MAX_BORROW = 7500;
+
     IERC20 public immutable weth;
+    IERC20 public immutable usdc;
     IPool public immutable pool;
     IPriceOracle public immutable oracle;
 
-    constructor(address _pap_addr, address _weth_addr) {
+    constructor(
+        address _pap_addr,
+        address _weth_addr,
+        address _usdc_addr
+    ) {
         IPoolAddressesProvider _pap = IPoolAddressesProvider(_pap_addr);
         pool = IPool(_pap.getPool());
         oracle = IPriceOracle(_pap.getPriceOracle());
         weth = IERC20(_weth_addr);
-        // TODO: Set emode for this contract
+        usdc = IERC20(_usdc_addr);
     }
 
     function deposit() external {}
@@ -41,22 +49,81 @@ contract AaveBot {
         if (_wethBalance >= 0.00001 ether) {
             weth.approve(address(pool), _wethBalance);
             pool.deposit(address(weth), _wethBalance, address(this), 0);
-            (, , , , , uint256 _health) = pool.getUserAccountData(
-                address(this)
-            );
 
-            if (_health >= 1.05 ether) {}
+            (
+                uint256 _totalCollateral,
+                uint256 _totalDebt,
+                uint256 _availableBorrows,
+                ,
+                ,
+                uint256 _health
+            ) = pool.getUserAccountData(address(this));
+
+            if (_health >= 1.05 ether) {
+                /* 
+                    new loan amount in eth = (totalDeposit * maxBorrowPercent) - existing_loans
+                */
+                (
+                    uint256 _loanMaxPercent,
+                    uint256 _liqThreshold
+                ) = getLoanThresholds(address(weth));
+
+                uint256 _newLoan = calcNewLoan(
+                    _totalCollateral,
+                    MAX_BORROW * 10e11
+                );
+
+                console.log(
+                    "Available Borrows USD %s, Available Borrows ETH %s, New Loans: %s",
+                    _availableBorrows,
+                    _priceToEth(_availableBorrows),
+                    _newLoan
+                );
+
+                pool.borrow(address(usdc), _newLoan, 1, 0, address(this));
+
+                console.log("================GOT HERE=================");
+            }
         }
     }
 
-    function reservesInEth() public view returns (uint256) {
+    function loansInEth() public view returns (uint256) {
+        (, uint256 _totalDebt, , , , ) = pool.getUserAccountData(address(this));
+        return _priceToEth(_totalDebt);
+    }
+
+    function depositsInEth() public view returns (uint256) {
         (uint256 _totalCollateral, , , , , ) = pool.getUserAccountData(
             address(this)
         );
+        return _priceToEth(_totalCollateral);
+    }
+
+    function _priceToEth(uint256 _priceInUsd) internal view returns (uint256) {
         return
             PRBMathUD60x18.div(
-                _totalCollateral,
+                _priceInUsd,
                 oracle.getAssetPrice(address(weth))
             );
+    }
+
+    function getLoanThresholds(address _asset)
+        public
+        view
+        returns (uint256, uint256)
+    {
+        uint256 bits = pool.getReserveData(_asset).configuration.data;
+        uint256 ltv = bits & LTV_BIT_MASK;
+        uint256 liqThresh = (bits >> 16) & LTV_BIT_MASK;
+
+        return (ltv, liqThresh);
+    }
+
+    function calcNewLoan(uint256 _deposits, uint256 _loanPercentage)
+        public
+        pure
+        returns (uint256)
+    {
+        return PRBMathUD60x18.mul(_deposits, _loanPercentage);
     }
 }
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -5,14 +5,17 @@ import "forge-std/Test.sol";
 import "src/Interfaces/IPool.sol";
 import "src/Interfaces/IPriceOracle.sol";
 import "prb-math/contracts/PRBMathUD60x18.sol";
+import "src/Libraries/DataTypes.sol";
 
 import "src/AaveBot.sol";
 
 contract ContractTest is Test {
     AaveBot bot;
     IERC20 weth;
+    IERC20 usdc;
     IPool pool;
     IPriceOracle oracle;
+    uint256 wethAmount = 1 ether;
 
     string ARBITRUM_RPC_URL = vm.envString("ALCHEMY_WEB_URL");
 
@@ -23,9 +26,12 @@ contract ContractTest is Test {
         vm.selectFork(forkId);
 
         weth = IERC20(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1);
+        usdc = IERC20(0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8);
+
         bot = new AaveBot(
             0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb,
-            address(weth)
+            address(weth),
+            address(usdc)
         );
 
         pool = IPool(bot.pool());
@@ -33,26 +39,50 @@ contract ContractTest is Test {
     }
 
     function testDepositToAave() public {
-        uint256 _wethAmount = 0.8 ether;
         uint256 _ethPrice = oracle.getAssetPrice(address(weth));
 
         console.log("Price of eth %s", _ethPrice);
 
         console.log(
             "Price of eth in contract %s",
-            PRBMathUD60x18.mul(_ethPrice, _wethAmount)
+            PRBMathUD60x18.mul(_ethPrice, wethAmount)
         );
 
-        // Simulate existing 1 eth deposit to bot
+        // Simulate eth deposit to bot
         vm.prank(address(weth));
-        weth.transfer(address(bot), _wethAmount);
+        weth.transfer(address(bot), wethAmount);
         uint256 _transferredWethAmount = weth.balanceOf(address(bot));
-        assertEq(_transferredWethAmount, _wethAmount);
+        assertEq(_transferredWethAmount, wethAmount);
 
         // AAVE USES 8 DECIMALS NOW (IDK WHY)
         // ALL RESERVES ARE QUOTED IN USD (IDK WHY)
         bot.main();
 
-        assertEq(bot.reservesInEth(), _wethAmount);
+        (
+            uint256 _totalCollateral,
+            uint256 _totalDebt,
+            uint256 _availableBorrows,
+            ,
+            ,
+            uint256 _health
+        ) = pool.getUserAccountData(address(bot));
+
+        assertEq(bot.depositsInEth(), wethAmount);
+        assertEq(usdc.balanceOf(address(bot)), 1237260000);
+    }
+
+    function test_ExtractReserveMap() public {
+        (uint256 loan, uint256 thresh) = bot.getLoanThresholds(address(weth));
+
+        console.log("LTV: %s, LIQ: %s", loan, thresh);
+
+        assertEq(loan, 8000);
+        assertEq(thresh, 8250);
+    }
+
+    function test_calcNewLoan() public {
+        uint256 result = bot.calcNewLoan(bot.depositsInEth(), 80 ether);
+
+        assertEq(result, PRBMathUD60x18.mul(bot.depositsInEth(), 80 ether));
     }
 }
